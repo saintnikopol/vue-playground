ad002be34318bc757f85d015122daf276524ae8f
saintnikopol <montesuma@gmail.com>
2018-06-21 21:59:17 +0300

    Author books implemented Pager draft added

--- new_dev__setup/src/BooksDatabase.js
+++ new_dev__setup/src/BooksDatabase.js
@@ -1,21 +1,46 @@
+const bookList = [
+    {id:'1',booksName:'Война и мир', author:'Лев Толстой', description:'В этой книге читатель вновь встретится с полюбившимися героями: среднестатистическим приспособленцем Артуром Дентом, сотрудником межгалактического издательства Фордом Префектом, продвинутой барышней Триллиан, депрессивным роботом Марвином, — и отправится вместе с ними в новые странствия по бесчисленным мирам, наполненным опасными приключениями и непредсказуемыми сюрпризами!'},
+    {id:'2',booksName:'«Автостопом по галактике» ', author:'Дуглас Адамс',description:'sdsdfasdf'},
+    {id:'3',booksName:'«Алиса в Стране чудес»', author:'Льюис Кэрролл',description:'wrgwrgq5hw'},
+    {id:'4',booksName:'«Алхимик» ', author:'Пауло Коэльо',description:'dfgadfgda'},
+    {id:'5',booksName:'«Белый пик»', author:'Энтони Хоровитц',description:'ergqergdrg'},
+    {id:'6',booksName:'«Большие надежды»', author:'Чарльз Диккенс',description:'ergaergq3g'},
+    {id:'7',booksName:'«Винни-Пух и все-все-все»', author:'Алан Милн',description:'ethqw4hwrtgdfv'},
+    {id:'8',booksName:'«Властелин колец»', author:'Джон Р. Р. Толкин',description:'aergaergq35g'},
+    {id:'9',booksName:'«Грозовой перевал»', author:'Эмили Бронте',description:'regq34gqdfvzdf'},
+    {id:'10',booksName:'«Дети полуночи» ', author:'Салман Рушди',description:'ea4gq3gdfg'},
+    {id:'11',booksName:'«Дневник Бриджит Джонс»', author:'Хелен Филдинг',description:'5yjdtyhdfgdf'},
+    {id:'12',booksName:'«Женщина в белом»', author:' Уильям Уилки Коллинз',description:'wrthw45hsg'},
+    {id:'13',booksName:'Война и мир часть 1', author:'Лев Толстой',description:'rthw45sgrwtw4 r tgrthert'},
+    {id:'14',booksName:'Война и мир часть 2', author:'Лев Толстой',description:'erthhw wrtgwrth'},
+    {id:'15',booksName:'Война и мир часть 3', author:'Лев Толстой',description:'rthw4tlk wergwerg wergwergerg'},
+    {id:'16', booksName:"«Мастер и Маргарита»", author:"Михаил Булгаков", description:"hdjfajhfdajhsd"}
+];
+
+
+
 
 let booksDatabase = {
-  bookList: [
-      {id:'1',booksName:'Война и мир', author:'Лев Толстой', description:'В этой книге читатель вновь встретится с полюбившимися героями: среднестатистическим приспособленцем Артуром Дентом, сотрудником межгалактического издательства Фордом Префектом, продвинутой барышней Триллиан, депрессивным роботом Марвином, — и отправится вместе с ними в новые странствия по бесчисленным мирам, наполненным опасными приключениями и непредсказуемыми сюрпризами!'},
-      {id:'2',booksName:'«Автостопом по галактике» ', author:'Дуглас Адамс',description:''},
-      {id:'3',booksName:'«Алиса в Стране чудес»', author:'Льюис Кэрролл',description:''},
-      {id:'4',booksName:'«Алхимик» ', author:'Пауло Коэльо',description:''},
-      {id:'5',booksName:'«Белый пик»', author:'Энтони Хоровитц',description:''},
-      {id:'6',booksName:'«Большие надежды»', author:'Чарльз Диккенс',description:''},
-      {id:'7',booksName:'«Винни-Пух и все-все-все»', author:'Алан Милн',description:''},
-      {id:'8',booksName:'«Властелин колец»', author:'Джон Р. Р. Толкин',description:''},
-      {id:'9',booksName:'«Грозовой перевал»', author:'Эмили Бронте',description:''},
-      {id:'10',booksName:'«Дети полуночи» ', author:'Салман Рушди',description:''},
-      {id:'11',booksName:'«Дневник Бриджит Джонс»', author:'Хелен Филдинг',description:''},
-      {id:'12',booksName:'«Женщина в белом»', author:' Уильям Уилки Коллинз',description:''},
-      {id:'13',booksName:'Война и мир часть 1', author:'Лев Толстой',description:''},
-      {id:'14',booksName:'Война и мир часть 2', author:'Лев Толстой',description:''},
-      {id:'15',booksName:'Война и мир часть 3', author:'Лев Толстой',description:''},
-  ]
+
+  bookList: bookList,
+
+  findBookById: (searchedId) => bookList.find(({id}) => id === searchedId),
+
+    findBooksByAuthor:(searchedAuthors) => bookList.filter(({author}) => author === searchedAuthors),
+
+    //creadNewArrayList: function(startList, endList){
+    //    let newArrayBooks = bookList.slice(startList, endList);
+    //    console.log(newArrayBooks, 'newArrayBooks')
+    //    return newArrayBooks;
+    //    newArrayBooks.push( bookList);
+    //},
+//
+
+//findBookById2: (searchedId) => bookList.find((item) => item.id === searchedId)
+  //findBookById3: function (searchedId) {
+  //    return  bookList.find((item) => item.id === searchedId)
+  //}
+
 };
+
 export default booksDatabase;
--- new_dev__setup/src/index.js
+++ new_dev__setup/src/index.js
@@ -3,58 +3,63 @@ console.log('Привет');
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
+var Paginate = require('vuejs-paginate')
+Vue.component('paginate', Paginate);
 
 import  * as booksDatabase from './BooksDatabase.js';
-let bookList = booksDatabase.default.bookList;
-console.log({bookList},{booksDatabase})
+let {bookList, findBookById, findBooksByAuthor, creadNewArrayList, greadstartBooksList}  = booksDatabase.default;
+//console.log({bookList},{booksDatabase});
 
 //import BooksText from './views/BooksText.vue';
 //import Authors from './views/Authors.vue';
-
+//let newArrayBooks = newArrayBooks;
 
 const BooksText = {
-  //methods: {
-  //  idBook: function() {
-  //    idB
-  //    //this.message = 'обновлено'
-  //    console.log(this.$el.id) // => 'не обновлено'
-  //    //this.$nextTick(function() {
-  //    //  console.log(this.$el.textContent) // => 'обновлено'
-  //    //})
-  //  },
-  //},
-  methods: {
-    idBookFind: function() {
-      console.log('idBook')
-      var idBook = this.$el.id;
-      //console.log(id);
-      console.log(idBook, 'idBook')
-    },
+  computed: {
+    description: function () {
+      let book = findBookById(this.id);
+      if (book) {
+        return book.description;
+
+      } else {
+        return '';
+      }
+    }
   },
-  props:['text'],
-  template: '<div><h2>Название книги</h2>{{text}}<div>Описание</div></div>',
+  props:['text', 'id'],
+  template: '<div><h2>Название книги</h2>{{text}}<div>Описание</div><div>' +
+  '{{description}}' +
+  '</div></div>',
 
 
 }
 
 const Authors = {
-  props:['authors'],
-  template: '<div><h2>Автор книги</h2> {{authors}}</div>'
+  computed: {
+    shortList: function () {
+      let shortList = findBooksByAuthor(this.author);
+      return shortList;
+
+    }
+  },
+  props:['author'],
+  // booksOfAuthor
+  template: '<div><h2>Автор книги</h2> {{author}}' +
+  '<ol >' +
+  '<li v-for="list in shortList">{{list.booksName}}</li></ol></div>'
 }
-
-
 
 const router = new VueRouter({
 
   routes: [
 
     {
-      path: '/authors/:authors',
+      path: '/authors/:author',
       component: Authors,
       props: true
     },
         {
-          path: '/authors/:authors/books/:text/id/:id',
+          path: '/authors/:author/books/:text/id/:id',
           component: BooksText,
           props: true,
         }
@@ -65,7 +70,24 @@ const router = new VueRouter({
 const app = new Vue({
   el:'#app',
   data:{
-  bookList:bookList
+    bookList:bookList
+  },
+  computed:{
+    visibleList: function(){
+      let startVisiblList = '0';
+        let endVisiblList = '15';
+       return this.bookList.slice(startVisiblList,endVisiblList);
+}
+  },
+  methods: {
+    clickCallback: function(pageNum) {
+      //console.log(bookList, 'clickCallback');
+      let endList = pageNum * '15';
+      let startList = endList - '15';
+      //console.log(bookList, 'clickCallback',endList,startList);
+      let newArrayBooks = this.bookList.slice(startList, endList);
+    },
+
   },
   router
 }).$mount('#app')
