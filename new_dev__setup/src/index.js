eecfb98452638df752cc1ac367214bffa906ef1f
saintnikopol <montesuma@gmail.com>
2018-06-22 08:59:06 +0300

    Pager works now

--- new_dev__setup/src/index.js
+++ new_dev__setup/src/index.js
@@ -2,8 +2,9 @@ console.log('Привет');
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
 Vue.use(VueRouter);
-var Paginate = require('vuejs-paginate')
+var Paginate = require('vuejs-paginate');
 Vue.component('paginate', Paginate);
 
 import  * as booksDatabase from './BooksDatabase.js';
@@ -22,34 +23,38 @@ const BooksText = {
       let book = findBookById(this.id);
       if (book) {
         return book.description;
-
       } else {
         return '';
       }
     }
   },
-  props:['text', 'id'],
-  template: '<div><h2>Название книги</h2>{{text}}<div>Описание</div><div>' +
-  '{{description}}' +
-  '</div></div>',
-
-
-}
+  props: ['text', 'id'],
+  template: '' +
+    '<div>' +
+      '<h2>Название книги</h2>{{text}}' +
+      '<div>Описание</div>' +
+    '<div>' +
+      '{{description}}' +
+    '</div></div>',
+};
 
 const Authors = {
   computed: {
     shortList: function () {
       let shortList = findBooksByAuthor(this.author);
       return shortList;
-
     }
   },
   props:['author'],
   // booksOfAuthor
-  template: '<div><h2>Автор книги</h2> {{author}}' +
-  '<ol >' +
-  '<li v-for="list in shortList">{{list.booksName}}</li></ol></div>'
-}
+  template: '<div>' +
+    '<h2>Автор книги</h2>' +
+      '{{author}}' +
+    '<ol >' +
+      '<li v-for="list in shortList">{{list.booksName}}</li>' +
+    '</ol>' +
+  '</div>',
+};
 
 const router = new VueRouter({
 
@@ -69,30 +74,41 @@ const router = new VueRouter({
   ]
 });
 
+const itemsPerPage = 5;
+const pageCount = Math.ceil(bookList.length / itemsPerPage);
+let pageNum = 1; // 0..max
+
+function calcLastItemIndex (pageNum) {
+  let lastPossibleItem = pageNum * itemsPerPage;
+  console.log(pageNum , 'pageNum ');
+  console.log(lastPossibleItem , 'calcLastItemIndex ');
+  return lastPossibleItem < bookList.length ? lastPossibleItem : bookList.length;
+}
+
 const app = new Vue({
   el:'#app',
-  data:{
-    bookList:bookList
+  data: {
+    bookList: bookList,
+    pageCount: pageCount,
+    currentPageNumber: pageNum,
+
   },
-  computed:{
-    visibleList: function(){
-      let startVisiblList = '0';
-        let endVisiblList = '15';
-       return this.bookList.slice(startVisiblList,endVisiblList);
-}
+  computed: {
+    visibleList: function() {
+      let startVisibleList = (this.currentPageNumber - 1) * itemsPerPage;
+      let endVisibleList = calcLastItemIndex(this.currentPageNumber);
+
+      return this.bookList.slice(startVisibleList, endVisibleList);
+    },
   },
   methods: {
-    clickCallback: function(pageNum) {
-      //console.log(bookList, 'clickCallback');
-      let endList = pageNum * '15';
-      let startList = endList - '15';
-      //console.log(bookList, 'clickCallback',endList,startList);
-      let newArrayBooks = this.bookList.slice(startList, endList);
+    clickCallback: function(newPageNum) {
+      this.currentPageNumber = newPageNum;
     },
 
   },
   router
-}).$mount('#app')
+}).$mount('#app');
 
 
 
